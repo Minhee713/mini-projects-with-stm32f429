4ba2804822c23062ec0f01ee9151b8f6951c73b2
min-hee Kwon <entwurf.k@gmail.com>
2024-01-30 20:20:13 +0900

    LCD포팅완료_버튼테스트중

--- rtos_alarm/Core/Src/main.c
+++ rtos_alarm/Core/Src/main.c
@@ -49,6 +49,8 @@ ETH_DMADescTypeDef  DMATxDscrTab[ETH_TX_DESC_CNT]; /* Ethernet Tx DMA Descriptor
 
 ETH_HandleTypeDef heth;
 
+I2C_HandleTypeDef hi2c1;
+
 UART_HandleTypeDef huart3;
 
 PCD_HandleTypeDef hpcd_USB_OTG_FS;
@@ -64,8 +66,10 @@ static void MX_GPIO_Init(void);
 static void MX_ETH_Init(void);
 static void MX_USART3_UART_Init(void);
 static void MX_USB_OTG_FS_PCD_Init(void);
+static void MX_I2C1_Init(void);
 void StartDefaultTask(void const * argument);
 
+static void MX_NVIC_Init(void);
 /* USER CODE BEGIN PFP */
 void USER_threads(void);
 /* USER CODE END PFP */
@@ -112,6 +116,10 @@ int main(void)
   MX_ETH_Init();
   MX_USART3_UART_Init();
   MX_USB_OTG_FS_PCD_Init();
+  MX_I2C1_Init();
+
+  /* Initialize interrupts */
+  MX_NVIC_Init();
   /* USER CODE BEGIN 2 */
   printf("Test: Main Start\n");
   /* USER CODE END 2 */
@@ -134,8 +142,8 @@ int main(void)
 
   /* Create the thread(s) */
   /* definition and creation of defaultTask */
-//  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
-//  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
   USER_threads();
@@ -203,6 +211,17 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief NVIC Configuration.
+  * @retval None
+  */
+static void MX_NVIC_Init(void)
+{
+  /* EXTI15_10_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+}
+
+/**
   * @brief ETH Initialization Function
   * @param None
   * @retval None
@@ -248,6 +267,54 @@ static void MX_ETH_Init(void)
   /* USER CODE BEGIN ETH_Init 2 */
 
   /* USER CODE END ETH_Init 2 */
+
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
 
 }
 
@@ -347,7 +414,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : USER_Btn_Pin */
   GPIO_InitStruct.Pin = USER_Btn_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(USER_Btn_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : LD1_Pin LD3_Pin LD2_Pin */
--- rtos_alarm/Core/Src/task.c
+++ rtos_alarm/Core/Src/task.c
@@ -20,9 +20,15 @@
 TaskHandle_t xHandleMain, xHandle1, xHandle2;
 
 void TaskMain(void);
+void Task1(void);
+void Task2(void);
+void Task3(void);
+void Task4(void);
+void Task5(void);
+
+
 
 /*Task functions*/
-
 void USER_threads(void)
 {
 	printf("User threads start!!\n");
@@ -32,8 +38,65 @@ void USER_threads(void)
 void TaskMain(void)
 {
 	const char* taskMainName = "TaskMain";
+	for (;;) {
+		printf("%s is running\r\n", taskMainName);
+	}
+	lcd_init();
+	xTaskCreate((TaskFunction_t)Task1, "Task1", 256, NULL, TASK_1_PRIO, &xHandle1);
+//	vTaskSuspend(xHandleMain);
+	//vTaskResume(xHandleMain);
 
-	printf("%s is running\r\n", taskMainName);
 }
+
+void Task1(void)
+{
+	const char* task1 = "Task1";
+	for (;;) {
+		printf("%s is running\r\n", task1);
+	}
+	lcd_test1();
+	vTaskSuspend(xHandle1);
+
+//	if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == 1) {
+//				HAL_GPIO_TogglePin(GPIOB, LD3_Pin);
+//	}
+
+
+
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+		if (GPIO_Pin == GPIO_PIN_13) {
+			HAL_GPIO_TogglePin(GPIOB, LD3_Pin);
+			vTaskSuspend(xHandleMain);
+
+			/*current_time = HAL_GetTick();
+			interval = current_time - last_time;
+			last_time = current_time;
+
+			if (HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_3) == 1) {
+				if (interval < 130) {
+					btn_cnt += 3;
+				} else if (interval >= 130 && interval < 200) {
+					printf("One click!!  interval = %u\r\n",
+							(unsigned int) interval);
+					btn_cnt = 0;
+					current_state.mode = TIME_SETTING;
+				} else if (interval >= 300 && interval <= 1000) {
+					printf("Long click!!  interval = %u\r\n",
+							(unsigned int) interval);
+					btn_cnt = 0;
+					current_state.mode = ALARM_TIME_SETTING;
+				}
+				if (btn_cnt >= 5) {
+					printf("Double click!!  interval = %u   btn_cnt = %d  \r\n",
+							(unsigned int) interval, btn_cnt);
+					btn_cnt = 0;
+					current_state.mode = MUSIC_SELECT;
+				}
+			}*/
+		}
+}
+
 
 
